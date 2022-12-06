579ce361a92c5c130eb34ea693fe708fb4c8dccf
Yagami_Light <1365745771@qq.com>
2022-12-06 23:07:03 +0800

    modify

--- src/Graph.java
+++ src/Graph.java
@@ -1,84 +1,119 @@
 import java.util.*;
+
 public class Graph {
-    int MinDegree,MaxDegree;// 图中最大/小度数
-    VertexSet vertices ;// 存点
-    //HashMap <Integer,Vertex>Id2Vex;
+    int MinDegree, MaxDegree;// 图中最大/小度数
+    VertexSet vertices;// 存点
+    HashMap <Integer,Vertex>Id2Vex;// 建立id到vertex的映射
+    // 获取该图中对应id的点信息
     HashMap<Integer, VertexSet> relation;
+
     //HashMap<Integer,Integer>CoreNumber; // cn[q]的值
     public Graph() {
-        vertices =  new VertexSet();
+        vertices = new VertexSet();
         relation = new HashMap<>();
-        MinDegree=MaxDegree=0;
+        Id2Vex = new HashMap<>();
+        MinDegree = MaxDegree = 0;
     }
-    public Graph(Graph G) {
-        vertices =new VertexSet(G.vertices);
+
+    public Graph(Graph G) {// 浅拷贝问题
+        vertices = new VertexSet(G.vertices);
         relation = new HashMap<>(G.relation);
-        this.MinDegree= G.MinDegree;
-        this.MaxDegree=G.MaxDegree;
+        Id2Vex = new HashMap<>(G.Id2Vex);
+        this.MinDegree = G.MinDegree;
+        this.MaxDegree = G.MaxDegree;
     }
-    void DataReader(){
+
+    void DataReader() {
         //input
+        buildIdMap();
     }
-    void CalDegree(){ // 计算每个点原图度数
-        MinDegree=MaxDegree= relation.get(vertices.Hset.iterator().next().id).Size();
+    public void buildIdMap(){
         for(Vertex v:vertices.Hset){
-            v.degree= relation.get(v.id).Size();
-            MinDegree=Math.min(MinDegree,v.degree);
-            MaxDegree=Math.max(MaxDegree,v.degree);
+            Id2Vex.put(v.id,v);
         }
     }
-    void CalCoreNum(){ // 计算所有k-core
-        HashMap<Integer , VertexSet>G= relation;
-        Queue<Vertex>que= new LinkedList<>() ;//
+    void CalDegree() { // 计算每个点原图度数 O(n)
+        MinDegree = MaxDegree = relation.get(vertices.Hset.iterator().next().id).Size();
+        for (Vertex v : vertices.Hset) {
+            v.degree = relation.get(v.id).Size();
+            MinDegree = Math.min(MinDegree, v.degree);
+            MaxDegree = Math.max(MaxDegree, v.degree);
+        }
+    }
+
+    void CalCoreNum() { // 计算所有k-core
+        HashMap<Integer, VertexSet> G = relation;
+        Queue<Vertex> que = new LinkedList<>();//
         //剩余点
-        HashSet<Vertex>vis=new HashSet<>();
+        HashSet<Vertex> vis = new HashSet<>();
         HashSet<Vertex> rest = new HashSet<>(vertices.Hset);
-        for(int k=1;k<=MaxDegree;k++){ // 计算 coreness
-            if(rest.isEmpty())break;
-            for(Vertex u:rest){
-                if(u.degree<k){
-                    if(!vis.contains(u)){
+        for (int k = 1; k <= MaxDegree; k++) { // 计算 coreness
+            if (rest.isEmpty()) break;
+            for (Vertex u : rest) {
+                if (u.degree < k) {
+                    if (!vis.contains(u)) {
                         vis.add(u);
-                        u.coreNumber=k-1;
+                        u.coreNumber = k - 1;
                         que.add(u);
                         rest.remove(u);
                     }
                 }
             }
-            while(que.size()>0){
-                Vertex u=que.poll();
-                for(Vertex v:G.get(u.id).Hset){
-                    if(--v.degree<k){
+            while (que.size() > 0) {
+                Vertex u = que.poll();
+                for (Vertex v : G.get(u.id).Hset) {
+                    if (--v.degree < k) {
                         que.add(v);
                         rest.remove(v);
-                        if(!vis.contains(u)) {
+                        if (!vis.contains(u)) {
                             vis.add(u);
-                            u.coreNumber=k-1;                        }
+                            u.coreNumber = k - 1;
+                        }
                     }
                 }
             }
         }
     }
-    void DelFromG(Vertex u){//从图中删除同时更新内部度
+
+    void DelFromGByInfo(Vertex u) {// 删除点u (信息 度数完全匹配(同一图中对应点))
+        //从图中删除同时更新内部度
+        if(!vertices.ID.contains(u.id))return; // 本来就没有
         relation.remove(u.id);
-        for(Vertex v:vertices.Hset){//
+        vertices.DelAll(u);
+        Id2Vex.remove(u.id);
+        for (Vertex v : vertices.Hset) {//
             relation.get(v.id).DelAll(v);
         }
-        for(Vertex v: relation.get(u.id).Hset){ // 更新度
+        for (Vertex v : relation.get(u.id).Hset) { // 更新度
             v.degree--;
         }
     }
-    void Add2G(Vertex u,Graph G){//从原图中加入
-        relation.put(u.id,new VertexSet());
-        for(Vertex v:vertices.Hset){//
-            if(G.relation.get(u.id).ID.contains(v.id)){
+    void DelFromGById(int id){// 删除id对应的同一个点 可能不同图中
+        //通过映射到图内信息实现
+        if(!vertices.ID.contains(id))return; // 本来就没有
+        Vertex v=Id2Vex.get(id);
+        DelFromGByInfo(v);
+    }
+
+        // 同理
+    void Add2GByInfo(Vertex u, Graph G) {//从原图中加入
+        if(vertices.ID.contains(u.id))return ; //本来就有
+        vertices.Add(u);
+        Id2Vex.put(u.id,u);
+        relation.put(u.id, new VertexSet());
+        for (Vertex v : vertices.Hset) {//
+            if (G.relation.get(u.id).ID.contains(v.id)) {
                 relation.get(v.id).Add2All(u);
                 relation.get(u.id).Add2All(v);
-                u.id++;
-                v.id++;
+                u.degree++;
+                v.degree++;
             }
+        }
+    }
 
-            }
-
+    void Add2GById(int id,Graph G){
+        if(vertices.ID.contains(id))return;
+        Vertex v= Id2Vex.get(id);
+        Add2GByInfo(v,G);
     }
 }
--- src/VetexSet.java
+++ src/VetexSet.java
@@ -3,14 +3,15 @@ import java.util.TreeSet;
 
 // 当某一步要开始使用Tset/Tset2时 先进行update1/2  然后使用Del/AddT1/T2方法
 class VertexSet {
-    HashSet<Integer>ID; // *用于查找 不同图中度数不同但还是同一个点
+    HashSet<Integer> ID; // *用于查找 不同图中度数不同但还是同一个点
     HashSet<Vertex> Hset;// hashset 用于简单无序存放
     TreeSet<Vertex> Tset;//
-    TreeSet<Vertex>Tset2;
+    TreeSet<Vertex> Tset2;
     Comparators Cmp;
+
     public VertexSet() {
         Cmp = new Comparators();
-        Hset = new HashSet<>();
+        ID = new HashSet<>();
         Hset = new HashSet<>();
         Tset = new TreeSet<>(Cmp.cmp1);
         Tset2 = new TreeSet<>(Cmp.cmp2);
@@ -31,25 +32,27 @@ class VertexSet {
     }
 
     public void Add(Vertex v) {
-        Hset.add(v);
+        Hset.add(new Vertex(v));
         ID.add(v.id);
     }
+
     public void AddT1(Vertex v) {
-        Hset.add(v);
+        Hset.add(new Vertex(v));
         ID.add(v.id);
-        Tset.add(v);
+        Tset.add(new Vertex(v));
     }
+
     public void AddT2(Vertex v) {
-        Hset.add(v);
+        Hset.add(new Vertex(v));
         ID.add(v.id);
-        Tset2.add(v);
+        Tset2.add(new Vertex(v));
     }
 
     public void Add2All(Vertex v) {
-        Hset.add(v);
+        Hset.add(new Vertex(v));
         ID.add(v.id);
-        Tset.add(v);
-        Tset2.add(v);
+        Tset.add(new Vertex(v));
+        Tset2.add(new Vertex(v));
     }
 
 
@@ -57,18 +60,21 @@ class VertexSet {
         Hset.remove(v);
         ID.remove(v.id);
     }
+
     public void DelT1(Vertex v) {
         Hset.remove(v);
         ID.remove(v.id);
         Tset.remove(v);
     }
+
     public void DelT2(Vertex v) {
         Hset.remove(v);
         ID.remove(v.id);
         Tset.remove(v);
         Tset2.remove(v);
     }
-    public void DelAll(Vertex v){
+
+    public void DelAll(Vertex v) {
         Hset.remove(v);
         ID.remove(v.id);
         Tset.remove(v);
@@ -79,6 +85,7 @@ class VertexSet {
         Tset = new TreeSet<>(Cmp.cmp1);
         Tset.addAll(Hset);
     }
+
     public void UpdateTset2() { // 若一直只对Hset操作
         Tset = new TreeSet<>(Cmp.cmp2);
         Tset2.addAll(Hset);
